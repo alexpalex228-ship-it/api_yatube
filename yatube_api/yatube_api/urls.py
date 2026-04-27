f08819be09e25dce7fa3f0d84fab2a064b4363b4
Alexey Nikolaev <alex_palex228@mail.ru>
2026-04-27 14:39:19 +0300

    Update urls.py

--- yatube_api/yatube_api/urls.py
+++ yatube_api/yatube_api/urls.py
@@ -3,9 +3,11 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('api/v1/', include('api.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
 if settings.DEBUG:
